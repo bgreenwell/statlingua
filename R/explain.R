a07b534c0f0596d4ac310fb8792ee6019713fe0e
bgreenwell <greenwell.brandon@gmail.com>
2025-05-13 19:56:19 -0400

    add pkgdown site

--- R/explain.R
+++ R/explain.R
@@ -29,17 +29,28 @@
 #'
 #' @examples
 #' \dontrun{
+#' # Polynomial regression
+#' cars_lm <- lm(dist ~ poly(speed, degree = 2), data = cars)
+#' context <- "
+#' The data give the speed of cars (mph) and the distances taken to stop (ft).
+#' Note that the data were recorded in the 1920s.
+#' "
+#' # Use Google Gemini to explain the output; requires an API key; see
+#' # ?ellmer::chat_gemini for details
+#' chat <- ellmer::chat_gemini()
+#' explain(cars_lm, chat = chat, context = context)
+#'
 #' # Poisson regression example from ?stats::glm
 #' counts <- c(18,17,15,20,10,20,25,13,12)
 #' outcome <- gl(3,1,9)
 #' treatment <- gl(3,3)
 #' data.frame(treatment, outcome, counts) # showing data
-#' glm.D93 <- glm(counts ~ outcome + treatment, family = poisson())
+#' D93_glm <- glm(counts ~ outcome + treatment, family = poisson())
 #'
 #' # Use Google Gemini to explain the output; requires an API key; see
 #' # ?ellmer::chat_gemini for details
 #' chat <- ellmer::chat_gemini()
-#' explain(glm.D93, chat = chat, verbose = TRUE)
+#' explain(D93_glm, chat = chat, verbose = TRUE)
 #' }
 #'
 #'
@@ -73,6 +84,43 @@ explain.htest <- function(object, chat, context = NULL, echo = NULL,
     usr_prompt,
     method = object$method,
     object = capture_output(object),
+    context = context
+  )
+  if (isTRUE(verbose)) {
+    message("System prompt:\n\n", sys_prompt)
+    message("Chatbot input:\n\n", usr_prompt)
+  }
+  chat$set_system_prompt(sys_prompt)
+  chat$chat(usr_prompt, echo = echo)
+}
+
+
+
+#' @rdname explain
+#' @export
+explain.lm <- function(object, chat, context = NULL, echo = NULL,
+                       verbose = FALSE, ...) {
+  stopifnot(inherits(chat, what = c("Chat", "R6")))
+  if (is.null(context)) {
+    context <- "No additional information available.\n"
+  }
+  path <- system.file("prompts/system_prompt_glm.md", package = "statlingua")
+  sys_prompt <- readChar(path, nchars = file.info(path)$size)
+  usr_prompt <-
+    "
+  Explain the output from the following linear regression model.
+
+  ## Model summary
+
+  {{model}}
+
+  ## Additional context
+
+  {{context}}
+  "
+  usr_prompt <- ellmer::interpolate(
+    usr_prompt,
+    model = capture_output(summary(object)),
     context = context
   )
   if (isTRUE(verbose)) {
